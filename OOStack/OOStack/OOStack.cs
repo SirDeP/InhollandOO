516edf12bd29dcb3df58eae3d27ef91c8c551204
SirDeP <biogene.53418@gmail.com>
2024-02-15 13:19:12 +0000

    upload OOStack

---
+++ OOStack/OOStack/OOStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStack
+{
+    internal class OOStack
+    {
+        const int maxAantalElementen = 10;
+        int aantal = 0;
+        int[] stack = new int[maxAantalElementen];
+        
+        public int Aantal
+        {
+            get { return aantal; }
+        }
+            
+        public void Push(int getal)
+        {
+            if (aantal < maxAantalElementen)
+            {
+                stack[aantal] = getal;
+                aantal++;
+            }
+            else
+            {
+                Console.WriteLine("Maximum aantal is bereikt");
+            }
+        }
+        public int Pop()
+        {
+            if (aantal > 0)
+            {
+                aantal--;
+                return stack[aantal];
+            }
+            else
+            {
+                Console.WriteLine("Stack is leeg");
+                return 0;
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.WriteLine(i + ":" + stack[i]);
+            }
+        }
+        
+    }
+}
---
+++ OOStack/OOStack/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OOStack MyStack = new OOStack();
+
+            String Commando = "";
+            while (Commando != "Stop")
+            {
+                Console.Write("Geef commando:");
+                Commando = Console.ReadLine();
+                if (Commando == "Push")
+                {
+                    Console.Write("Geef Getal:");
+                    MyStack.Push(int.Parse(Console.ReadLine()));
+                }
+                else if (Commando == "Pop")
+                {
+                    Console.WriteLine(MyStack.Pop());
+                }
+                else if (Commando == "Print")
+                    MyStack.Print();
+                else if (Commando == "Aantal")
+                    Console.WriteLine(MyStack.Aantal + " element(en)");
+            }
+        }
+    }
+}
