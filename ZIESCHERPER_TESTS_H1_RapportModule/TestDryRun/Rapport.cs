96fec2cd5d3f7bec2b919336f0149e230e6af9c9
SirDeP <biogene.53418@gmail.com>
2024-02-23 10:13:20 +0100

    initial upload

---
+++ ZIESCHERPER_TESTS_H1_RapportModule/TestDryRun/Rapport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapportModule
+{
+    public class Rapport
+    {
+        public int Percentage { get; set; }
+        public void PrintGraad()
+        {
+            if (Percentage < 50)
+                Console.WriteLine("Niet geslaagd");
+            else if (Percentage >= 50 && Percentage <= 68)
+                Console.WriteLine("Voldoende");
+            else if (Percentage > 68 && Percentage <= 75)
+                Console.WriteLine("Onderscheiding");
+            else if (Percentage > 75 && Percentage <= 85)
+                Console.WriteLine("Grote onderscheiding");
+            else if (Percentage > 85)
+                Console.WriteLine("Grootste onderscheiding");
+        }
+    }
+}
