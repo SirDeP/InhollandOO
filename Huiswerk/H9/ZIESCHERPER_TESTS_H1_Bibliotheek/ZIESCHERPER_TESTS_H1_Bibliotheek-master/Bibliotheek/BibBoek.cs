c62739050158ad92aed29a179b8e7323869c7029
SirDeP <biogene.53418@gmail.com>
2024-02-24 13:48:02 +0100

    upload third half

---
+++ Huiswerk/H9/ZIESCHERPER_TESTS_H1_Bibliotheek/ZIESCHERPER_TESTS_H1_Bibliotheek-master/Bibliotheek/BibBoek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheek
+{
+    public class BibBoek
+    {
+        private const int AANTALUITLEENDAGEN = 14;
+        public string Ontlener { get; set; } = "onbekend";
+
+        private DateTime uitgeleend = DateTime.Now;
+
+        public DateTime Uitgeleend
+        {
+            private get { return uitgeleend; }
+            set { uitgeleend = value; }
+        }
+
+        //public DateTime Uitgeleend { private get; set; } = DateTime.Now;
+        private DateTime inleverdatum;
+
+        public DateTime InleverDatum
+        {
+            get 
+            { 
+                inleverdatum = Uitgeleend.AddDays(AANTALUITLEENDAGEN);
+                return inleverdatum; 
+            }
+        }
+
+
+        public void VerlengTermijn(int Dagen)
+        {
+            Uitgeleend = Uitgeleend.AddDays(Dagen);
+        }
+    }
+}
---
+++ Huiswerk/H9/ZIESCHERPER_TESTS_H1_Persoon/ZIESCHERPER_TESTS_H1_Persoon-master/Persoon/Persoon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persoon
+{
+    public class Persoon
+    {
+        public string Achternaam { get; set; }
+        public string Voornaam { get; set; }
+
+        private DateTime geboortedatum;
+        DateTime minimum = new DateTime(1990, 1, 1);
+        public DateTime GeboorteDatum
+        {
+            get { return geboortedatum; }
+            set 
+            { 
+                if (value < minimum)
+                    geboortedatum = DateTime.Now;
+                else
+                    geboortedatum = value;
+            }
+        }
+        public int BerekenLeeftijd()
+        {
+            DateTime today = DateTime.Today;
+            int leeftijd = today.Year - GeboorteDatum.Year;
+            if (today.Month < GeboorteDatum.Month || (today.Month == GeboorteDatum.Month && today.Day < GeboorteDatum.Day))
+                leeftijd--;
+            return leeftijd;
+        }
+
+    }
+}
