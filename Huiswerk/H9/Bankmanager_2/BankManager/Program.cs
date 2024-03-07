6d544d410e03314b81a678732ac8f69eb5b1e0fc
SirDeP <biogene.53418@gmail.com>
2024-03-07 10:16:15 +0100

    yee

---
+++ Huiswerk/H9/Bankmanager_2/BankManager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rekening toby = new Rekening();
+            Rekening sjaak = new Rekening();
+            sjaak = sjaak.CreeerTienerRekening("Sjaak Bakker");
+            toby = toby.CreeerTienerRekening("Toby Fox");
+            toby.ToonInfo();
+            sjaak.ToonInfo();
+            SimuleerOverdracht(sjaak, toby);
+            toby.ToonInfo();
+            sjaak.ToonInfo();
+
+        }
+
+        static void SimuleerOverdracht(Rekening rekening1, Rekening rekening2)
+        {
+            Random rnd = new Random();
+            rekening2.StortGeld(rekening1.HaalGeldAf(rnd.Next(1, 11)));
+            rekening1.StortGeld(rekening2.HaalGeldAf(rnd.Next(1, 11)));
+            rekening2.StortGeld(rekening1.HaalGeldAf(rnd.Next(1, 11)));
+            rekening1.StortGeld(rekening2.HaalGeldAf(rnd.Next(1, 11)));
+            rekening2.StortGeld(rekening1.HaalGeldAf(rnd.Next(1, 11)));
+        }
+
+
+    }
+}
---
+++ Huiswerk/H9/Bankmanager_2/BankManager/Rekening.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManager
+{
+    public enum RekeningStaat { Geblokkeerd, Geldig }
+    public class Rekening
+    {
+        int balans;
+        public string NaamKlant { get; set; }
+        public string RekeningNummer { get; set; }
+        public RekeningStaat Staat { get; private set; } = RekeningStaat.Geldig;
+        public int Balans
+        {
+            get { return balans; }
+        }
+
+        public int HaalGeldAf(int bedrag)
+        {
+            if (Staat == RekeningStaat.Geblokkeerd)
+                Console.WriteLine("Deze rekening is geblokkeerd");
+            if (balans >= bedrag)
+            {
+                balans -= bedrag;
+                return bedrag;
+            }
+            else if (balans < bedrag)
+            {
+                int temp = Balans;
+                balans = 0;
+                Console.WriteLine("Rekening leeg nu");
+                Staat = RekeningStaat.Geblokkeerd;
+                return temp;
+            }
+            return 0;
+        }
+
+        public void StortGeld(int bedrag)
+        {
+            if (Staat == RekeningStaat.Geblokkeerd)
+                Console.WriteLine("Deze rekening is geblokkeerd");
+            else
+            balans += bedrag;
+        }
+
+        public void VeranderStaat()
+        {
+            if (Staat == RekeningStaat.Geldig)
+                Staat = RekeningStaat.Geblokkeerd;
+            else
+                Staat = RekeningStaat.Geldig;
+        }
+
+        public void ToonInfo()
+        {
+            Console.WriteLine($"Naam klant: {NaamKlant}");
+            Console.WriteLine($"Rekeningnummer: {RekeningNummer}");
+            Console.WriteLine($"Balans: {balans}");
+            Console.WriteLine($"Staat: {Staat}");
+        }
+        public Rekening CreeerTienerRekening(string naamklant)
+        {
+            Rekening tienerrekening = new Rekening();
+            Random rnd = new Random();
+            for (int i = 0; i < 11; i++)
+                tienerrekening.RekeningNummer += rnd.Next(1, 9).ToString();
+            tienerrekening.NaamKlant = naamklant;
+            tienerrekening.StortGeld(50);
+            return tienerrekening;
+        }
+    }
+}
+
