617570713e1d1fc8b4d65a7c84d2738fb22fc9bb
SirDeP <biogene.53418@gmail.com>
2024-03-21 12:59:13 +0000

    Add UML

---
+++ VPProjects/OOStack.cpp
@@ -0,0 +1,21 @@
+#include "OOStack.h"
+
+int OOStack::Aantal() {
+	// TODO - implement OOStack::Aantal
+	throw "Not yet implemented";
+}
+
+int OOStack::Pop() {
+	// TODO - implement OOStack::Pop
+	throw "Not yet implemented";
+}
+
+void OOStack::Push(int getal) {
+	// TODO - implement OOStack::Push
+	throw "Not yet implemented";
+}
+
+void OOStack::Print() {
+	// TODO - implement OOStack::Print
+	throw "Not yet implemented";
+}
